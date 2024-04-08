916fedf0e7651eac99db74ab600bf7b1e5dde9f8
MajorTimberWolf <ak0929@srmist.edu.in>
2024-04-09 00:38:33 +0530

    feat: add tokio function definition
    This commit adds tokio add functionality along with ipv6 server listening

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,39 @@
-mod proto{
-    
+use proto::calculator_server::{Calculator, CalculatorServer};
+use tonic::{transport::Server, Request};
+mod proto {
+    tonic::include_proto!("calculator");
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, Default)]
+struct CalculatorService {}
+
+#[tonic::async_trait]
+impl Calculator for CalculatorService {
+    async fn add(
+        &self,
+        request: tonic::Request<proto::CalculationRequest>,
+    ) -> Result<tonic::Response<proto::CalculationReponse>, tonic::Status> {
+        println!("Got a request: {:?}", request);
+
+        let input = request.get_ref();
+
+        let response = proto::CalculationReponse {
+            result: input.a + input.b,
+        };
+
+        Ok(tonic::Response::new(response))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let addr = "[::1]:50051".parse()?;
+    
+    let calc = CalculatorService::default();
+
+    Server::builder()
+        .add_service(CalculatorService::new(calc))
+        .serve(addr)
+        .await?;
+    Ok(())
 }
